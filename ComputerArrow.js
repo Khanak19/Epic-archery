332476a35647144ed646a5ccf7bac91719cfd60b
Khanak19 <85015299+Khanak19@users.noreply.github.com>
2021-06-24 21:15:50 +0530

    Add files via upload

--- ComputerArrow.js
+++ ComputerArrow.js
@@ -9,9 +9,14 @@ class ComputerArrow {
     this.width = width;
     this.height = height;
     this.body = Bodies.rectangle(x, y, this.width, this.height, options);
-    this.image = loadImage("arrow.png");
+    this.image = loadImage("./assets/arrow.png");
+    this.isRemoved = false;
+    this.archerAngle = archerAngle;
+    this.velocity = p5.Vector.fromAngle(archerAngle);
     World.add(world, this.body);
   }
+
+
   shoot(archerAngle) {
     this.velocity = p5.Vector.fromAngle(archerAngle + PI / 2);
     this.velocity.mult(32);
@@ -22,7 +27,17 @@ class ComputerArrow {
 
     Matter.Body.setStatic(this.body, false);
   }
- display() {
+
+  display() {
+    var tmpAngle;
+    if (this.body.velocity.y === 0) {
+      tmpAngle = this.archerAngle + PI / 2;
+    } else {
+      tmpAngle = Math.atan(this.body.velocity.y / this.body.velocity.x) - PI;
+    }
+
+    Matter.Body.setAngle(this.body, tmpAngle);
+
     var pos = this.body.position;
     var angle = this.body.angle;
 
@@ -32,7 +47,5 @@ class ComputerArrow {
     imageMode(CENTER);
     image(this.image, 0, 0, this.width, this.height);
     pop();
-
   }
-
 }
